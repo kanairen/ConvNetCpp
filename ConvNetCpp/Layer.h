2e8937d4292c5773624559a63593a7f07c89ba1c
kanairen <turutekab@gmail.com>
2016-05-07 19:24:52 +0900

    clean Layer::backword()の引数名を修正

--- ConvNetCpp/Layer.h
+++ ConvNetCpp/Layer.h
@@ -15,21 +15,21 @@ private:
     vector<vector<float>> *weights;
     // バイアス変数
     vector<float> *biases;
-
+    
     // 入力の重み付き和
     vector<float> *u;
     // 順伝播出力
     vector<float> *z;
     // 逆伝播デルタ
     vector<float> *delta;
-
+    
     // デフォルトコンストラクタを明示的に無効化
     Layer();
     // コピーコンストラクタを無効化
     Layer(const Layer& otherLayer);
     // 代入演算子の無効化
     Layer& operator=(const Layer& otherLayer);
-
+    
 public:
     // 引数付きコンストラクタ
     Layer(int n_in, int n_out);
@@ -40,18 +40,18 @@ public:
     vector<vector<float>>* getWeights();
     vector<float>* getBiases();
     vector<float>* getDelta();
-
+    
     // 順伝播関数
     vector<float>* forward(vector<float> *x);
     // 逆伝播関数
-    vector<float>* backward(vector<float> *nextDelta, vector<vector<float>> *nextWeight);
+    vector<float>* backward(vector<float> *next_delta, vector<vector<float>> *next_weight);
     // パラメタ更新
     void update(vector<float> *delta);
     // 活性化関数
     virtual float activation(float x);
     // 活性化関数微分形
     virtual float gradActivation(float x);
-
+    
     virtual string toString();
 };
 
