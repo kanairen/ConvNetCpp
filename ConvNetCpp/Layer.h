1a3bc205e74ea259c87f0c192d971c52acdd243e
kanairen <turutekab@gmail.com>
2016-05-23 22:41:20 +0900

    modify Layer.hをEigenで再実装

--- ConvNetCpp/Layer.h
+++ ConvNetCpp/Layer.h
@@ -2,60 +2,70 @@
 //  Layer.h
 //  ConvNetCpp
 //
-//  Created by 金井廉 on 2016/05/06.
-//  Copyright © 2016年 金井廉. All rights reserved.
+//  Created by ren on 2016/05/06.
+//  Copyright © 2016年 ren. All rights reserved.
 //
 
 #ifndef LAYER_H_
 #define LAYER_H_
 
-#include <stdio.h>
 #include <iostream>
 #include <sstream>
 #include <random>
+#include <Eigen/Core>
 
 #include "Activation.h"
 
 using namespace std;
+using namespace Eigen;
 
-class Layer{
+class Layer {
 private:
-    
+
     unsigned int n_in, n_out;
     float learning_rate;
-    
+
     Activation *activation;
-    
-    vector<vector<float>*> *weights;
-    vector<float> *biases;
-    
+
+    MatrixXf *weights;
+    VectorXf *biases;
+
     /* 計算結果を格納する配列・変数*/
-    vector<float> *u;
-    vector<float> *z;
-    vector<float> *delta;
-    float b_delta;
-   
-    Layer();
-    Layer(const Layer& otherLayer);
-    Layer(unsigned int n_in, unsigned int n_out, Activation *activation, float learning_rate);
-    Layer& operator=(const Layer& otherLayer);
-    
+    MatrixXf *u;
+    MatrixXf *z;
+    MatrixXf *delta;
+
+    Layer(unsigned int n_in, unsigned int n_out, Activation *activation,
+          float learning_rate);
+
     void update();
-    
+
 public:
     virtual ~Layer();
-    
-    static Layer* newLayer(unsigned int n_in, unsigned int n_out, Activation *activation, float learning_rate){return new Layer(n_in, n_out, activation, learning_rate);}
-    
-    int getNIn(){return n_in;}
-    int getNOut(){return n_out;}
-    vector<vector<float>*>* getWeights(){return weights;}
-    vector<float>* getBiases(){return biases;}
-    vector<float>* getDelta(){return delta;}
-    
-    virtual vector<float>* forward(vector<float> *x);
-    void backward(vector<float> *nextDelta, vector<vector<float>*> *nextWeight);// 逆伝播関数
-    
+
+    static Layer *newLayer(unsigned int n_in, unsigned int n_out,
+                           Activation *activation, float learning_rate) {
+        return new Layer(n_in, n_out, activation, learning_rate);
+    }
+
+    int getNIn() { return n_in; }
+
+    int getNOut() { return n_out; }
+
+    MatrixXf *getWeights() { return weights; }
+
+    VectorXf *getBiases() { return biases; }
+
+    MatrixXf *getU() { return u; }
+
+    MatrixXf *getZ() { return z; }
+
+    MatrixXf *getDelta() { return delta; }
+
+    virtual MatrixXf *forward(MatrixXf *x);
+
+    void backward(MatrixXf *nextDelta, MatrixXf *nextWeight);// 逆伝播関数
+
     virtual string toString();
 };
 
