b1b0ada1db61ed6dce306ec247db422c3f0b64ae
kanairen <turutekab@gmail.com>
2016-06-14 17:10:42 +0900

    fix test_layer.cpp

--- tests/TestLayer.cpp
+++ tests/TestLayer.cpp
@@ -4,18 +4,19 @@
 
 #include "TestLayer.h"
 
+// 関数
+float identity(float x) {
+    return x;
+}
+
 void test_layer() {
 
-    // 関数
-    float identity(float x) {
-        return x;
-    }
 
     // 入力データ
     vector<vector<float>> v(5, vector<float>(10, 1));
 
     // Layerオブジェクト
-    Layer l(5, 10, 10, identity);
+    Layer l(10, 5, 10, identity);
 
     // 順伝播
     const vector<vector<float>> &output = l.forward(v);
