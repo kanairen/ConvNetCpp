c2822b4f78a82fd5e4f465b96e9c83c3401f25b8
kanairen <turutekab@gmail.com>
2016-06-18 13:24:01 +0900

    add MNIST用テストケースを追加

---
+++ tests/TestMNIST.h
@@ -0,0 +1,15 @@
+//
+// Created by kanairen on 2016/06/15.
+//
+
+#ifndef CONVNETCPP_TEST_MNIST_H
+#define CONVNETCPP_TEST_MNIST_H
+
+#include "../src/MNIST.h"
+#include <unistd.h>
+#include <iomanip>
+#include <cassert>
+
+void test_mnist();
+
+#endif //CONVNETCPP_TEST_MNIST_H
