235503ced8091daff14f9dac714856846b5cc90b
kanairen <turutekab@gmail.com>
2016-06-18 13:23:23 +0900

    fix Layer用テストケースを修正

--- tests/TestLayer.cpp
+++ tests/TestLayer.cpp
@@ -9,17 +9,19 @@ float identity(float x) {
     return x;
 }
 
-void test_layer() {
+float grad_identity(float x) {
+    return 1;
+}
 
+void test_layer() {
 
     // 入力データ
     vector<vector<float>> v(5, vector<float>(10, 1));
 
     // Layerオブジェクト
-    Layer l(10, 5, 10, identity);
+    Layer l(10, 5, 10, identity, grad_identity);
 
     // 順伝播
     const vector<vector<float>> &output = l.forward(v);
-
 
 }
--- tests/suite.cpp
+++ tests/suite.cpp
@@ -2,8 +2,14 @@
 // Created by kanairen on 2016/06/14.
 //
 
-#include "TestLayer.h"
+#ifndef DEBUG
 
-int main(){
-    test_layer();
-}
+#include "TestMNIST.h"
+#include "TestLayer.h"
+//
+//int main(){
+//    test_mnist();
+//    test_layer();
+//}
+
+#endif
