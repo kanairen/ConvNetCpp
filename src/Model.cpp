3ccb168437488c36ff4131b00fdd365378304588
kanairen <turutekab@gmail.com>
2016-06-18 13:24:48 +0900

    add 学習モデルクラスModelを追加

---
+++ src/Model.cpp
@@ -0,0 +1,88 @@
+//
+// Created by kanairen on 2016/06/14.
+//
+
+#include "Model.h"
+
+vector<vector<float>> &Model::forward(vector<vector<float>> &inputs) {
+    vector<vector<float>> &output = inputs;
+    for (int i = 0; i < layers.size(); i++) {
+        output = layers[i].forward(output);
+    }
+    return output;
+}
+
+void Model::backward(vector<vector<float>> &inputs,
+                     vector<vector<float>> &last_delta, float learning_rate) {
+
+    layers.back().backward(last_delta, layers[layers.size() - 2].get_z(),
+                           learning_rate);
+    for (int i = layers.size() - 2; i > 0; --i) {
+        layers[i].backward(layers[i + 1], layers[i - 1].get_z(), learning_rate);
+    }
+    layers[0].backward(layers[1], inputs, learning_rate);
+}
+
+void Model::softmax(const vector<vector<float>> &outputs,
+                    vector<vector<float>> &y) {
+
+    if (outputs.size() != y.size() && outputs[0].size() != y[0].size()) {
+        std::cerr << "error :  Model::softmax()" << endl;
+        exit(1);
+    }
+
+    float u, sum_exp;
+    for (int i = 0; i < outputs.size(); ++i) {
+        sum_exp = 0.f;
+        for (int j = 0; j < outputs[i].size(); ++j) {
+            u = exp(outputs[i][j]);
+            y[i][j] = u;
+            sum_exp += u;
+        }
+        for (int k = 0; k < outputs[i].size(); ++k) {
+            y[i][k] /= sum_exp;
+        }
+    }
+
+}
+
+void Model::argmax(const vector<vector<float>> &y, vector<int> &predict) {
+    if (y[0].size() != predict.size()) {
+        std::cerr << "error :  Model::argmax()" << endl;
+        exit(1);
+    }
+
+    float max, tmp;
+    int max_idx;
+    for (int i = 0; i < y[0].size(); ++i) {
+        max = y[0][i];
+        max_idx = 0;
+        for (int j = 1; j < y.size(); ++j) {
+            tmp = y[j][i];
+            if (tmp > max) {
+                max = tmp;
+                max_idx = j;
+            }
+        }
+        predict[i] = max_idx;
+    }
+
+}
+
+float Model::error(const vector<int> &predict, const vector<int> &answer) {
+
+    if (predict.size() != answer.size()) {
+        std::cerr << "error :  Model::error()" << endl;
+        exit(1);
+    }
+
+    float num_error = 0.;
+    for (int i = 0; i < predict.size(); ++i) {
+        if (predict[i] != answer[i]) {
+            num_error += 1;
+        }
+    }
+
+    return num_error / predict.size();
+
+}
---
+++ src/Model.h
@@ -0,0 +1,43 @@
+//
+// Created by kanairen on 2016/06/14.
+//
+
+#ifndef CONVNETCPP_MODEL_H
+#define CONVNETCPP_MODEL_H
+
+#include <cmath>
+#include <vector>
+#include "Layer.h"
+
+using std::vector;
+
+class Model {
+private:
+    vector<Layer> &layers;
+    vector<vector<float>> out_forward;
+public:
+    Model(vector<Layer> &layers, unsigned int n_data) :
+            layers(layers),
+            out_forward(vector<vector<float>>(layers.back().get_n_out(),
+                                              vector<float>(n_data, 0.f))) { };
+
+    ~Model() { };
+
+    vector<vector<float>> &forward(vector<vector<float>> &inputs);
+
+    void backward(vector<vector<float>> &inputs,
+                  vector<vector<float>> &last_delta, float learning_rate);
+
+    static void softmax(const vector<vector<float>> &outputs,
+                        vector<vector<float>> &y);
+
+    static void argmax(const vector<vector<float>> &y,
+                       vector<int> &predict);
+
+    static float error(const vector<int> &predict, const vector<int> &answer);
+
+
+};
+
+
+#endif //CONVNETCPP_MODEL_H
