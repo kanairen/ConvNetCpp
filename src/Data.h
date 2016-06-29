6ab64202c52201f829421461f5eeebb6ee10c239
kanairen <turutekab@gmail.com>
2016-06-29 13:32:39 +0900

    add Data.h

---
+++ src/Data.h
@@ -0,0 +1,31 @@
+//
+// Created by kanairen on 2016/06/29.
+//
+
+#ifndef CONVNETCPP_DATA_H
+#define CONVNETCPP_DATA_H
+
+#include <sstream>
+#include <vector>
+
+using std::vector;
+
+template<class X, class Y>
+class DataSet {
+public:
+    vector<vector<X>> x_train;
+    vector<vector<X>> x_test;
+    vector<Y> y_train;
+    vector<Y> y_test;
+
+    std::string toString() {
+        std::stringstream ss;
+        ss << "x_train : " << x_train.size();
+        ss << " x_test : " << x_test.size();
+        ss << " y_train : " << y_train.size();
+        ss << " y_test : " << y_test.size();
+        return ss.str();
+    }
+};
+
+#endif //CONVNETCPP_DATA_H
