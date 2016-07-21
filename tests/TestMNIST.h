7c584d0138712b03a20ec1de7fc2105e18099022
kanairen <turutekab@gmail.com>
2016-07-21 23:01:55 +0900

    clean tests

--- tests/TestMNIST.h
+++ tests/TestMNIST.h
@@ -10,6 +10,34 @@
 #include <iomanip>
 #include <cassert>
 
-void test_mnist();
+void test_mnist() {
+    MNIST mnist("./x_train", "./x_test", "./y_train", "./y_test");
+
+    assert(mnist.x_train.size() == mnist.y_train.size());
+    assert(mnist.x_test.size() == mnist.y_test.size());
+
+    // train
+    for (int i = 0; i < 5; ++i) {
+        cout << mnist.y_train[i] << endl;
+        for (int row = 0; row < 28; ++row) {
+            for (int col = 0; col < 28; ++col) {
+                cout << std::setw(4) << mnist.x_train[i][row * 28 + col] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    // test
+    for (int i = 0; i < 5; ++i) {
+        cout << mnist.y_test[i] << endl;
+        for (int row = 0; row < 28; ++row) {
+            for (int col = 0; col < 28; ++col) {
+                cout << std::setw(4) << mnist.x_test[i][row * 28 + col] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+}
 
 #endif //CONVNETCPP_TEST_MNIST_H
--- tests/suite.cpp
+++ tests/suite.cpp
@@ -2,16 +2,3 @@
 // Created by kanairen on 2016/06/14.
 //
 
-#include "../src/config.h"
-
-#ifndef CONV_NET_CPP_DEBUG
-
-#include "TestMNIST.h"
-#include "TestLayer.h"
-
-int main(){
-    test_mnist();
-    test_layer();
-}
-
-#endif
