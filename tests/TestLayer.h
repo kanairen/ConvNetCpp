2bd30b20c45ba656be984424f480bd9f866db811
kanairen <turutekab@gmail.com>
2016-07-27 18:47:58 +0900

    modify TestLayer using GoogleTest.

--- tests/TestLayer.h
+++ tests/TestLayer.h
@@ -5,218 +5,149 @@
 #ifndef CONVNETCPP_TESTLAYER_H
 #define CONVNETCPP_TESTLAYER_H
 
+#include<gtest/gtest.h>
 #include "../src/activation.h"
 #include "../src/Layer.h"
 
-#undef NODEBUG
+class LayerTest : public ::testing::Test {
+protected:
 
-namespace layer {
+    Layer_ *layer;
 
-    void test_init() {
-
-        std::cout << "TestLayer::test_init()... " << std::endl;
-
-        const unsigned int n_data = 5;
-        const unsigned int n_in = 3;
-        const unsigned int n_out = 4;
-        float (*activation)(float) = iden;
-        float (*grad_activation)(float) = g_iden;
-
-        Layer_ layer(n_data, n_in, n_out, activation, grad_activation);
-
-        const MatrixXf &weights = layer.get_weights();
-        const VectorXf &biases = layer.get_biases();
-        const MatrixXf &delta = layer.get_delta();
-        const MatrixXf &z = layer.get_z();
-
-        assert(weights.rows() == n_out);
-        assert(weights.cols() == n_in);
-
-        assert(biases.size() == n_out);
-
-        assert(delta.rows() == n_out);
-        assert(delta.cols() == n_data);
-
-        assert(z.rows() == n_out);
-        assert(z.cols() == n_data);
-
-        std::cout << "weights : " << std::endl;
-        std::cout << weights << std::endl;
-
-        std::cout << "biases : " << std::endl;
-        std::cout << layer.get_biases() << std::endl;
-
-        std::cout << "delta : " << std::endl;
-        std::cout << layer.get_delta() << std::endl;
-
-        std::cout << "z : " << std::endl;
-        std::cout << layer.get_z() << std::endl;
-
+    virtual void SetUp() {
+        std::cout << "TestLayer::SetUp()" << std::endl;
     }
 
-    void test_forward() {
+public:
 
-        std::cout << "TestLayer::test_forward()... " << std::endl;
+    unsigned int n_data;
+    unsigned int n_in;
+    unsigned int n_out;
 
-        const unsigned int n_data = 5;
-        const unsigned int n_in = 3;
-        const unsigned int n_out = 4;
-        float (*activation)(float) = iden;
-        float (*grad_activation)(float) = g_iden;
+    float (*activation)(float);
 
-        Layer_ layer(n_data, n_in, n_out, activation, grad_activation, false,
-                     1);
+    float (*grad_activation)(float);
 
-        MatrixXf input(n_in, n_data);
-        input << 1, 4, 7, 10, 13,
-                2, 5, 8, 11, 14,
-                3, 6, 9, 12, 15;
+    LayerTest() : n_data(5), n_in(3), n_out(4),
+                  activation(iden), grad_activation(g_iden),
+                  layer(new Layer_(5, 3, 4, iden, g_iden, false, 1.f)) { }
 
-        std::cout << "output : " << std::endl;
-        std::cout << layer.forward(input) << std::endl;
-
-        Layer layer_vec(n_data, n_in, n_out, activation, grad_activation, false,
-                        1);
-
-        // Layer(vector ver)::forward()
-        int count = 0;
-        vector<float> in(n_in * n_data);
-        for (int j = 0; j < n_data; ++j) {
-            for (int i = 0; i < n_in; ++i) {
-                in[i * n_data + j] = ++count;
-            }
-        }
-
-        for (int i = 0; i < n_in; ++i) {
-            for (int j = 0; j < n_data; ++j) {
-                std::cout << in[i * n_data + j] << " ";
-            }
-            std::cout << std::endl;
-        }
-
-        const vector<float> &out = layer_vec.forward(in);
-
-        std::cout << "output(vector ver) : " << std::endl;
-        for (int i = 0; i < n_out; ++i) {
-            for (int j = 0; j < n_data; ++j) {
-                std::cout << out[i * n_data + j] << " ";
-            }
-            std::cout << std::endl;
-        }
-
-
+    ~LayerTest() {
+        delete layer;
     }
 
-    void test_backward() {
+};
 
-        std::cout << "TestLayer::test_backward()... " << std::endl;
+TEST_F(LayerTest, test_init) {
 
-        const unsigned int n_data = 3;
-        const unsigned int n_in = 2;
-        const unsigned int n_out = 4;
-        const unsigned int next_n_out = 6;
-        float (*activation)(float) = iden;
-        float (*grad_activation)(float) = g_iden;
+    std::cout << "TestLayer::test_init()... " << std::endl;
 
-        Layer_ layer(n_data, n_in, n_out, activation, grad_activation, false,
-                     1);
+    const MatrixXf &weights = layer->get_weights();
+    const VectorXf &biases = layer->get_biases();
+    const MatrixXf &delta = layer->get_delta();
+    const MatrixXf &z = layer->get_z();
 
-        MatrixXf input(n_in, n_data);
-        input << 1, 3, 5,
-                2, 4, 6;
+    ASSERT_EQ(weights.rows(), n_out);
+    ASSERT_EQ(weights.cols(), n_in);
 
-        MatrixXf &&next_weight = (MatrixXf &&) MatrixXf::Ones(next_n_out,
-                                                              n_out);
-        MatrixXf &&next_delta = (MatrixXf &&) MatrixXf::Ones(next_n_out,
-                                                             n_data);
+    ASSERT_EQ(biases.size(), n_out);
 
-        std::cout << "forward : " << std::endl;
-        std::cout << layer.forward(input) << std::endl;
+    ASSERT_EQ(delta.rows(), n_out);
+    ASSERT_EQ(delta.cols(), n_data);
 
-        // test for forward
-        MatrixXf result_forward(n_out, n_data);
-        result_forward << 3, 7, 11,
-                3, 7, 11,
-                3, 7, 11,
-                3, 7, 11;
+    ASSERT_EQ(z.rows(), n_out);
+    ASSERT_EQ(z.cols(), n_data);
 
-        assert(layer.forward(input) == result_forward);
+    std::cout << "weights : " << std::endl;
+    std::cout << weights << std::endl;
 
-        std::cout << "backward : " << std::endl;
-        layer.backward(next_weight, next_delta, input, n_out, 1);
+    std::cout << "biases : " << std::endl;
+    std::cout << biases << std::endl;
 
-        // test for backward
-        MatrixXf &&result_delta = (MatrixXf &&) MatrixXf::Constant(n_out,
-                                                                   n_data,
-                                                                   6);
+    std::cout << "delta : " << std::endl;
+    std::cout << delta << std::endl;
 
-        assert(layer.get_delta() == result_delta);
+    std::cout << "z : " << std::endl;
+    std::cout << z << std::endl;
 
-        MatrixXf &&result_weight = (MatrixXf &&) MatrixXf(n_out, n_in);
-        result_weight << -17, -23,
-                -17, -23,
-                -17, -23,
-                -17, -23;
-
-        assert(layer.get_weights() == result_weight);
-
-        VectorXf &&result_biases = (VectorXf &&) VectorXf(n_out);
-        result_biases << -6, -6, -6, -6;
-
-        assert(layer.get_biases() == result_biases);
-
-
-        // test for forward again.
-
-        result_forward << -69, -149, -229,
-                -69, -149, -229,
-                -69, -149, -229,
-                -69, -149, -229;
-
-        assert(layer.forward(input) == result_forward);
-
-        std::cout << "forward : " << std::endl;
-        std::cout << layer.forward(input) << std::endl;
-
-
-        // Layer(vector ver)::forward()
-        Layer layer_vec(n_data, n_in, n_out, activation, grad_activation, false,
-                        1.f);
-
-        vector<float> in{1, 3, 5, 2, 4, 6};
-
-        vector<float> next_w(next_n_out * n_out, 1.f);
-        vector<float> next_d(next_n_out * n_data, 1.f);
-
-        std::cout << "output(vector ver) : " << std::endl;
-        const vector<float> &out = layer_vec.forward(in);
-        for (int i = 0; i < n_out; ++i) {
-            for (int j = 0; j < n_data; ++j) {
-                std::cout << out[i * n_data + j] << " ";
-            }
-            std::cout << std::endl;
-        }
-
-        layer_vec.backward(next_w, next_d, in, next_n_out, 1.f);
-
-        std::cout << "delta(vector ver) : " << std::endl;
-        const vector<float> &delta_vec = layer_vec.get_delta();
-        for (int i = 0; i < n_out; ++i) {
-            for (int j = 0; j < n_data; ++j) {
-                std::cout << delta_vec[i * n_data + j] << " ";
-            }
-            std::cout << std::endl;
-        }
-
-        std::cout << "weight_after(vector ver) : " << std::endl;
-        const vector<float> &w_vec = layer_vec.get_weights();
-         for (int i = 0; i < n_out; ++i) {
-            for (int j = 0; j < n_in; ++j) {
-                std::cout << w_vec[i * n_in + j] << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
 }
+
+TEST_F(LayerTest, test_forward) {
+
+    std::cout << "TestLayer::test_forward()... " << std::endl;
+
+    MatrixXf input(n_in, n_data);
+    input << 1, 4, 7, 10, 13,
+            2, 5, 8, 11, 14,
+            3, 6, 9, 12, 15;
+
+    const MatrixXf &output = layer->forward(input);
+
+    std::cout << "output : " << std::endl;
+    std::cout << output << std::endl;
+
+    MatrixXf result_forward(n_out, n_data);
+    result_forward << 3, 7, 11,
+            3, 7, 11,
+            3, 7, 11,
+            3, 7, 11;
+
+    ASSERT_EQ(output, result_forward);
+
+}
+
+TEST_F(LayerTest, test_backward) {
+
+    std::cout << "TestLayer::test_backward()... " << std::endl;
+
+    MatrixXf input(n_in, n_data);
+    input << 1, 3, 5,
+            2, 4, 6;
+
+    const unsigned int next_n_out = 4;
+
+    MatrixXf &&next_weight = (MatrixXf &&) MatrixXf::Ones(next_n_out, n_out);
+    MatrixXf &&next_delta = (MatrixXf &&) MatrixXf::Ones(next_n_out, n_data);
+
+    std::cout << "forward : " << std::endl;
+    std::cout << layer->forward(input) << std::endl;
+
+    std::cout << "backward : " << std::endl;
+    layer->backward(next_weight, next_delta, input, n_out, 1.f);
+
+    // test for backward
+    MatrixXf &&result_delta = (MatrixXf &&) MatrixXf::Constant(n_out, n_data,
+                                                               6.f);
+
+    ASSERT_EQ(layer->get_delta(), result_delta);
+
+    MatrixXf &&result_weight = (MatrixXf &&) MatrixXf(n_out, n_in);
+    result_weight << -17, -23,
+            -17, -23,
+            -17, -23,
+            -17, -23;
+
+    ASSERT_EQ(layer->get_weights(), result_weight);
+
+    VectorXf &&result_biases = (VectorXf &&) VectorXf(n_out);
+    result_biases << -6, -6, -6, -6;
+
+    ASSERT_EQ(layer->get_biases(), result_biases);
+
+    // test for forward again.
+
+    MatrixXf result_forward(n_out, n_data);
+    result_forward << -69, -149, -229,
+            -69, -149, -229,
+            -69, -149, -229,
+            -69, -149, -229;
+
+    ASSERT_EQ(layer->forward(input), result_forward);
+
+    std::cout << "forward : " << std::endl;
+    std::cout << layer->forward(input) << std::endl;
+
+}
+
 #endif //CONVNETCPP_TESTLAYER_H
 
--- tests/suite.cpp
+++ tests/suite.cpp
@@ -12,11 +12,6 @@
 
 int main() {
 
-    // test Layer
-    layer::test_init();
-    layer::test_forward();
-    layer::test_backward();
-
     // test Softmax Layer
     sm_layer::test_init();
     sm_layer::test_forward();
