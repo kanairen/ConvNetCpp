d1c06769a48001fde85f676dd1ca28398deb200d
kanairen <turutekab@gmail.com>
2016-07-28 12:50:38 +0900

    modify TestConvLayer using GoogleTest.

--- tests/TestConvLayer.h
+++ tests/TestConvLayer.h
@@ -8,181 +8,204 @@
 #include "../src/ConvLayer.h"
 #include "../src/activation.h"
 
-namespace conv_layer {
+class ConvLayer2dTest : public ::testing::Test {
+protected:
+    static const unsigned int N_DATA;
+    static const unsigned int INPUT_WIDTH;
+    static const unsigned int INPUT_HEIGHT;
+    static const unsigned int C_IN;
+    static const unsigned int C_OUT;
+    static const unsigned int KW;
+    static const unsigned int KH;
+    static const unsigned int SX;
+    static const unsigned int SY;
+    static const unsigned int PX;
+    static const unsigned int PY;
 
-    void test_init() {
+    constexpr static float (*const ACTIVATION)(float) = iden;
 
-        std::cout << "TestConvLayer::test_init()... " << std::endl;
+    constexpr static float (*const GRAD_ACTIVATION)(float) = g_iden;
 
-        unsigned int n_data = 3;
-        unsigned int input_width = 3;
-        unsigned int input_height = 3;
-        unsigned int c_in = 1;
-        unsigned int c_out = 2;
-        unsigned int kw = 2;
-        unsigned int kh = 2;
-        unsigned int sx = 1;
-        unsigned int sy = 1;
-        unsigned int px = 0;
-        unsigned int py = 0;
-        float (*activation)(float) = iden;
-        float (*grad_activation)(float) = g_iden;
+    static const bool IS_WEIGHT_INIT_ENABLED = false;
 
-        ConvLayer2d_ layer(n_data, input_width, input_height, c_in, c_out, kw,
-                           kh, sx, sy, px, py, activation, grad_activation);
+    ConvLayer2d_ *layer;
 
-        const MatrixXf &weights = layer.get_weights();
-        const VectorXf &biases = layer.get_biases();
-        const MatrixXf &delta = layer.get_delta();
-        const MatrixXf &z = layer.get_z();
+    ConvLayer2dTest() : layer(
+            new ConvLayer2d_(N_DATA, INPUT_WIDTH, INPUT_HEIGHT, C_IN, C_OUT,
+                             KW, KH, SX, SY, PX, PY, ACTIVATION,
+                             GRAD_ACTIVATION, IS_WEIGHT_INIT_ENABLED, 1.f)) { }
 
-        const unsigned int n_in = layer.get_n_in();
-        const unsigned int n_out = layer.get_n_out();
+    virtual ~ConvLayer2dTest() {
+        delete layer;
+    }
 
-        std::cout << "weights : " << std::endl;
-        std::cout << weights << std::endl;
+    virtual void SetUp() {
+        std::cout << "ConvLayer2dTest::SetUp()" << std::endl;
+    }
 
-        std::cout << "biases : " << std::endl;
-        std::cout << layer.get_biases() << std::endl;
+    virtual void TearDown() {
+        std::cout << "ConvLayer2dTest::TearDown()" << std::endl;
+    }
 
-        std::cout << "delta : " << std::endl;
-        std::cout << layer.get_delta() << std::endl;
+};
 
-        std::cout << "z : " << std::endl;
-        std::cout << layer.get_z() << std::endl;
+const unsigned int ConvLayer2dTest::N_DATA = 1;
+const unsigned int ConvLayer2dTest::INPUT_WIDTH = 3;
+const unsigned int ConvLayer2dTest::INPUT_HEIGHT = 3;
+const unsigned int ConvLayer2dTest::C_IN = 1;
+const unsigned int ConvLayer2dTest::C_OUT = 2;
+const unsigned int ConvLayer2dTest::KW = 2;
+const unsigned int ConvLayer2dTest::KH = 2;
+const unsigned int ConvLayer2dTest::SX = 1;
+const unsigned int ConvLayer2dTest::SY = 1;
+const unsigned int ConvLayer2dTest::PX = 0;
+const unsigned int ConvLayer2dTest::PY = 0;
 
-        assert(weights.rows() == n_out);
-        assert(weights.cols() == n_in);
+TEST_F(ConvLayer2dTest, test_init) {
 
-        const auto weight_sum = weights.rowwise().sum();
-        bool is_nonzero_exists;
-        for (int i = 0; i < weights.rows(); ++i) {
-            is_nonzero_exists = false;
-            for (int j = 0; j < weights.cols(); ++j) {
-                // TODO 比較方法を修正すべき
-                if (weights(i, j) != 0.f) {
-                    is_nonzero_exists = true;
-                }
+    std::cout << "ConvLayer2dTest::test_init()... " << std::endl;
+
+    const MatrixXf &weights = layer->get_weights();
+    const VectorXf &biases = layer->get_biases();
+    const MatrixXf &delta = layer->get_delta();
+    const MatrixXf &z = layer->get_z();
+
+    const unsigned int n_in = layer->get_n_in();
+    const unsigned int n_out = layer->get_n_out();
+
+    std::cout << "weights : " << std::endl;
+    std::cout << weights << std::endl;
+
+    std::cout << "biases : " << std::endl;
+    std::cout << biases << std::endl;
+
+    std::cout << "delta : " << std::endl;
+    std::cout << delta << std::endl;
+
+    std::cout << "z : " << std::endl;
+    std::cout << z << std::endl;
+
+    ASSERT_EQ(weights.rows(), n_out);
+    ASSERT_EQ(weights.cols(), n_in);
+
+    bool is_nonzero_exists;
+    for (int i = 0; i < weights.rows(); ++i) {
+        is_nonzero_exists = false;
+        for (int j = 0; j < weights.cols(); ++j) {
+            // TODO 比較方法を修正すべき
+            if (weights(i, j) != 0.f) {
+                is_nonzero_exists = true;
             }
-            assert(is_nonzero_exists);
         }
-
-        assert(biases.size() == n_out);
-
-        assert(delta.rows() == n_out);
-        assert(delta.cols() == n_data);
-
-        assert(z.rows() == n_out);
-        assert(z.cols() == n_data);
-
+        ASSERT_TRUE(is_nonzero_exists);
     }
 
+    ASSERT_EQ(biases.size(), n_out);
 
-    void test_backward() {
+    ASSERT_EQ(delta.rows(), n_out);
+    ASSERT_EQ(delta.cols(), N_DATA);
 
-        std::cout << "TestConvLayer::test_backward()... " << std::endl;
-
-        unsigned int n_data = 1;
-        unsigned int input_width = 3;
-        unsigned int input_height = 3;
-        unsigned int c_in = 1;
-        unsigned int c_out = 2;
-        unsigned int kw = 2;
-        unsigned int kh = 2;
-        unsigned int sx = 1;
-        unsigned int sy = 1;
-        unsigned int px = 0;
-        unsigned int py = 0;
-        float (*activation)(float) = iden;
-        float (*grad_activation)(float) = g_iden;
-
-        ConvLayer2d_ layer(n_data, input_width, input_height, c_in, c_out, kw,
-                           kh, sx, sy, px, py, activation, grad_activation,
-                           false, 1.f);
-
-        unsigned int next_n_out = 4;
-        unsigned int n_out = layer.get_n_out();
-        unsigned int n_in = layer.get_n_in();
-
-        MatrixXf input(input_width * input_height, 1);
-        input << 1,
-                2,
-                3,
-                4,
-                5,
-                6,
-                7,
-                8,
-                9;
-
-
-        std::cout << "weights : " << std::endl;
-        std::cout << layer.get_weights() << std::endl;
-
-        MatrixXf &&next_weight = (MatrixXf &&) MatrixXf::Ones(next_n_out,
-                                                              n_out);
-        MatrixXf &&next_delta = (MatrixXf &&) MatrixXf::Ones(next_n_out,
-                                                             n_data);
-
-        std::cout << "forward : " << std::endl;
-        std::cout << layer.forward(input) << std::endl;
-
-        // test for forward
-        MatrixXf result_forward(n_out, n_data);
-        result_forward <<
-        12,
-                16,
-                24,
-                28,
-
-                12,
-                16,
-                24,
-                28;
-
-        assert(layer.forward(input) == result_forward);
-
-        std::cout << "backward : " << std::endl;
-        layer.backward(next_weight, next_delta, input, n_out, 1);
-
-        std::cout << "delta : " << std::endl;
-        std::cout << layer.get_delta() << std::endl;
-
-        // test for backward
-        MatrixXf &&result_delta = (MatrixXf &&) MatrixXf::Constant(n_out,
-                                                                   n_data,
-                                                                   4);
-
-        assert(layer.get_delta() == result_delta);
-
-        MatrixXf &&res_w = (MatrixXf &&) MatrixXf(n_out, n_in);
-        res_w << -47, -63, 0, -95, -111, 0, 0, 0, 0,
-                0, -47, -63, 0, -95, -111, 0, 0, 0,
-                0, 0, 0, -47, -63, 0, -95, -111, 0,
-                0, 0, 0, 0, -47, -63, 0, -95, -111,
-                -47, -63, 0, -95, -111, 0, 0, 0, 0,
-                0, -47, -63, 0, -95, -111, 0, 0, 0,
-                0, 0, 0, -47, -63, 0, -95, -111, 0,
-                0, 0, 0, 0, -47, -63, 0, -95, -111;
-
-        std::cout << "weights : " << std::endl;
-        std::cout << layer.get_weights() << std::endl;
-
-        assert(layer.get_weights() == res_w);
-
-        VectorXf &&result_biases = (VectorXf &&) VectorXf(n_out);
-        result_biases << -4, -4, -4, -4, -4, -4, -4, -4;
-
-        std::cout << "biases : " << std::endl;
-        std::cout << layer.get_biases() << std::endl;
-
-        assert(layer.get_biases() == result_biases);
-
-        std::cout << "forward : " << std::endl;
-        std::cout << layer.forward(input) << std::endl;
-
-    }
+    ASSERT_EQ(z.rows(), n_out);
+    ASSERT_EQ(z.cols(), N_DATA);
 
 }
+
+
+TEST_F(ConvLayer2dTest, test_backward) {
+
+    std::cout << "ConvLayer2dTest::test_backward()... " << std::endl;
+
+    unsigned int next_n_out = 4;
+    unsigned int n_out = layer->get_n_out();
+    unsigned int n_in = layer->get_n_in();
+
+    MatrixXf input(INPUT_WIDTH * INPUT_HEIGHT * C_IN, 1);
+    input << 1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9;
+
+    std::cout << "weights : " << std::endl;
+    std::cout << layer->get_weights() << std::endl;
+
+    // test forward
+    const MatrixXf &output = layer->forward(input);
+
+    std::cout << "forward : " << std::endl;
+    std::cout << output << std::endl;
+
+    MatrixXf result_output(n_out, N_DATA);
+    result_output <<
+    12,
+            16,
+            24,
+            28,
+
+            12,
+            16,
+            24,
+            28;
+
+    ASSERT_EQ(output, result_output);
+
+    // test backward
+    MatrixXf &&next_weight = (MatrixXf &&) MatrixXf::Ones(next_n_out, n_out);
+    MatrixXf &&next_delta = (MatrixXf &&) MatrixXf::Ones(next_n_out, N_DATA);
+
+    std::cout << "backward : " << std::endl;
+    layer->backward(next_weight, next_delta, input, n_out, 1);
+
+    // test delta
+    const MatrixXf &delta = layer->get_delta();
+
+    std::cout << "delta : " << std::endl;
+    std::cout << delta << std::endl;
+
+    MatrixXf &&result_delta = (MatrixXf &&) MatrixXf::Constant(n_out, N_DATA,
+                                                               4);
+
+    ASSERT_EQ(delta, result_delta);
+
+
+    //test learned weights
+    const MatrixXf &learned_weights = layer->get_weights();
+
+    std::cout << "weights : " << std::endl;
+    std::cout << learned_weights << std::endl;
+
+    MatrixXf &&result_learned_weights = (MatrixXf &&) MatrixXf(n_out, n_in);
+    result_learned_weights << -47, -63, 0, -95, -111, 0, 0, 0, 0,
+            0, -47, -63, 0, -95, -111, 0, 0, 0,
+            0, 0, 0, -47, -63, 0, -95, -111, 0,
+            0, 0, 0, 0, -47, -63, 0, -95, -111,
+            -47, -63, 0, -95, -111, 0, 0, 0, 0,
+            0, -47, -63, 0, -95, -111, 0, 0, 0,
+            0, 0, 0, -47, -63, 0, -95, -111, 0,
+            0, 0, 0, 0, -47, -63, 0, -95, -111;
+
+    ASSERT_EQ(learned_weights, result_learned_weights);
+
+    // test learned biases
+    const VectorXf &learned_biases = layer->get_biases();
+
+    std::cout << "biases : " << std::endl;
+    std::cout << learned_biases << std::endl;
+
+    VectorXf &&result_biases = (VectorXf &&) VectorXf(n_out);
+    result_biases << -4, -4, -4, -4, -4, -4, -4, -4;
+
+    ASSERT_EQ(learned_biases, result_biases);
+
+    // check forward after learning
+    std::cout << "forward : " << std::endl;
+    std::cout << layer->forward(input) << std::endl;
+
+}
+
 
 #endif //CONVNETCPP_TESTCONVLAYER_H
