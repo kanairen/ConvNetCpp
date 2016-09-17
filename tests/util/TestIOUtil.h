468d13fa99f28996b20190236e4ad9f2163db27d
kanairen <turutekab@gmail.com>
2016-09-17 16:01:38 +0900

    modify TestIOUtilの標準出力先をstringstreamに変更し、文字列チェックを追加

--- tests/util/TestIOUtil.h
+++ tests/util/TestIOUtil.h
@@ -5,10 +5,11 @@
 #ifndef CONVNETCPP_TESTIOUTIL_H
 #define CONVNETCPP_TESTIOUTIL_H
 
-#include "stdio.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
+#include <stdio.h>
 #include <gtest/gtest.h>
 #include "../../src/util/IOUtil.h"
 
@@ -34,7 +35,51 @@ protected:
 
 TEST_F(IOUtilTest, test_print) {
     std::cout << "IOUtilTest::test_print()" << std::endl;
+
+    std::stringbuf s_buf;
+    std::streambuf *prev_buf = std::cout.rdbuf(&s_buf);
+
     print(v);
+
+    std::cout.rdbuf(prev_buf);
+
+    std::string result = "";
+    for (int elem : v) {
+        result += std::to_string(elem) + ", ";
+    }
+    result += "\n";
+
+    ASSERT_EQ(s_buf.str(), result);
+
+}
+
+TEST_F(IOUtilTest, test_print_col) {
+    std::cout << "IOUtilTest::test_print()" << std::endl;
+
+    std::stringbuf s_buf;
+    std::streambuf *prev_buf = std::cout.rdbuf(&s_buf);
+
+    std::vector<std::vector<int>> v = {{1, 2, 3},
+                                       {4, 5, 6}};
+
+    int col = 0;
+
+    print_col(v, col);
+
+    std::cout.rdbuf(prev_buf);
+
+    std::string result = "";
+    for (const std::vector<int> &v_row : v) {
+        for (int i = 0; i < v_row.size(); ++i) {
+            if (i == col) {
+                result += std::to_string(v_row[i]) + ", ";
+            }
+        }
+    }
+    result += "\n";
+
+    ASSERT_EQ(s_buf.str(), result);
+
 }
 
 TEST_F(IOUtilTest, test__save_as_csv) {
@@ -44,7 +89,7 @@ TEST_F(IOUtilTest, test__save_as_csv) {
 
     std::ifstream ifs(path);
     std::string line;
-    while(getline(ifs, line)){
+    while (getline(ifs, line)) {
         std::cout << line << std::endl;
     }
 
