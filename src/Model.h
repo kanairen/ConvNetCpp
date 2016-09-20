1f039d361e7f23eab1c3f3935a92b51b9b71b8d7
kanairen <turutekab@gmail.com>
2016-09-20 19:57:30 +0900

    remove std::vector実装のModelを消去

--- src/Model.h
+++ src/Model.h
@@ -12,33 +12,6 @@
 using Eigen::VectorXi;
 using std::vector;
 
-class Model {
-private:
-    vector<Layer *> &layers;
-    vector<float> out_forward;
-
-    Model() = delete;
-
-public:
-    Model(vector<Layer *> &layers, unsigned int n_data) :
-            layers(layers),
-            out_forward(vector<float>(layers.back()->get_n_out() * n_data,
-                                      0.f)) { };
-
-    ~Model() { };
-
-    const vector<float> &forward(const vector<float> &inputs);
-
-    void backward(const vector<float> &inputs,
-                  const vector<float> &last_delta, float learning_rate);
-
-    static void argmax(const vector<float> &y, vector<int> &predict,
-                       const unsigned int n_out, const unsigned int n_data);
-
-    static float error(const vector<int> &predict, const vector<int> &answer);
-
-
-};
 
 class Model_ {
 private:
