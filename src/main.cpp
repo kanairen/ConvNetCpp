440fb0399828d64c07f377b414d5a8270b9c01b8
kanairen <turutekab@gmail.com>
2016-09-28 20:10:50 +0900

    add XMLによるmain.cpp上の設定のDIを追加

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <map>
 
 #include "config.h"
+#include "Data.h"
 #include "MNIST.h"
 #include "ShapeMap.h"
 #include "layer/SoftMaxLayer.h"
@@ -13,245 +14,266 @@
 #include "Model.h"
 #include "Optimizer.h"
 
+#include "tinyxml/tinyxml2.h"
+
+using tinyxml2::XMLDocument;
+using tinyxml2::XMLElement;
+using tinyxml2::XMLNode;
+
 #ifdef CONV_NET_CPP_DEBUG
 
-namespace cnc {
 
-    const float LEARNING_RATE = 0.01f;
+class DataSetHelper {
+private:
 
-    const unsigned int BATCH_SIZE = 25;
+    DataSetHelper() = delete;
 
-    const unsigned int MNIST_WIDTH = 28;
-    const unsigned int MNIST_HEIGHT = 28;
-    const unsigned int INPUT_SIZE = MNIST_WIDTH * MNIST_HEIGHT;
+    DataSetHelper(const DataSetHelper &dataset_helper) = delete;
 
-    const unsigned int N_ITERATION = 1000;
+    virtual ~DataSetHelper() = default;
 
-    // full connect layer
-    const unsigned int N_HIDDEN_UNITS = 10000;
+public:
 
-    float (*const LAYER_ACTIVATION)(float) = relu;
+    enum Type : long {
+        MNIST_ = 0x00,
+        BAND_SHAPE_MAP = 0x01,
+    };
 
-    float (*const LAYER_GRAD_ACTIVATION)(float) = g_relu;
-
-    // grid layer
-    const unsigned int C_IN = 1;
-    const unsigned int C_OUT = 16;
-
-    const unsigned int KERNEL_WIDTH = 2;
-    const unsigned int KERNEL_HEIGHT = 2;
-
-    const unsigned int STRIDE_X = 1;
-    const unsigned int STRIDE_Y = 1;
-
-    const unsigned int PADDING_X = 0;
-    const unsigned int PADDING_Y = 0;
-
-    float (*const CONV_ACTIVATION)(float) = iden;
-
-    float (*const CONV_GRAD_ACTIVATION)(float) = g_iden;
-
+    static DataSet<float, int> *get_dataset(int id, char *argv[]) {
+        switch (id) {
+            case MNIST_:
+                return new MNIST(argv[2], argv[3], argv[4], argv[5]);
+            case BAND_SHAPE_MAP:
+                return new ShapeMapSet(argv[2], argv[3]);
+            default:
+                error_and_exit(
+                        "DataSetHelper::get_dataset : failed to get dataset.");
+                return nullptr;
+        }
+    }
 
 };
 
+typedef float (*ACTIVATION)(float);
 
-void mnist_full_connect_eigen(char *argv[]) {
+class ActivationHelper {
+private:
 
-    string f_x_train = argv[2];
-    string f_x_test = argv[3];
-    string f_y_train = argv[4];
-    string f_y_test = argv[5];
+    ActivationHelper() = delete;
 
-    // mnist
-    MNIST mnist(f_x_train, f_x_test, f_y_train, f_y_test);
+    ActivationHelper(const ActivationHelper &activation_helper) = delete;
 
-    unsigned int n_class = mnist.get_n_cls();
+    virtual ~ActivationHelper() = default;
 
-    Layer_ *layer_1 = new Layer_(cnc::BATCH_SIZE,
-                                 cnc::INPUT_SIZE,
-                                 cnc::N_HIDDEN_UNITS,
-                                 cnc::LAYER_ACTIVATION,
-                                 cnc::LAYER_GRAD_ACTIVATION);
+public:
 
-    Layer_ *layer_2 = new SoftMaxLayer_(cnc::BATCH_SIZE,
-                                        layer_1->get_n_out(),
-                                        n_class);
+    enum Type : long {
+        SIGMOID = 0x00,
+        RELU = 0x01,
+    };
 
-    vector<Layer_ *> v{layer_1, layer_2};
+    static ACTIVATION get_activation(int id) {
+        switch (id) {
+            case SIGMOID:
+                return sigmoid;
+            case RELU:
+                return relu;
+            default:
+                error_and_exit(
+                        "ActivationHelper::get_activation() : failed to get activate function.");
+                return nullptr;
+        }
+    }
 
-    // optimize
-    optimize_(mnist, v, cnc::LEARNING_RATE, cnc::BATCH_SIZE,
-              cnc::N_ITERATION, n_class, argv[6], argv[7]);
+    static ACTIVATION get_g_activation(int id) {
+        switch (id) {
+            case SIGMOID:
+                return g_sigmoid;
+            case RELU:
+                return g_relu;
+            default:
+                error_and_exit(
+                        "ActivationHelper::get_grad_activation() : failed to get activate function.");
+                return nullptr;
+        }
+    }
 
-    // release
-    delete layer_1;
-    delete layer_2;
+};
+
+namespace xmlkey {
+    constexpr char FULL_CONNECT[] = "full_connect";
+    constexpr char SOFTMAX[] = "softmax";
+
+    constexpr char N_HIDDEN[] = "n_hidden";
+    constexpr char ACTIVATION_ID[] = "activation_id";
+    constexpr char IS_WEIGHT_RAND_INIT_ENABLED[] = "is_weight_rand_init_enabled";
+    constexpr char WEIGHT_CONSTANT_VALUE[] = "weight_constant_value";
+    constexpr char IS_DROPOUT_ENABLED[] = "is_dropout_enabled";
+    constexpr char DROPOUT_RATE[] = "dropout_rate";
 }
 
-void mnist_conv_eigen(char *argv[]) {
+std::map<string, string> params_common(XMLElement *elem) {
 
-    string f_x_train = argv[2];
-    string f_x_test = argv[3];
-    string f_y_train = argv[4];
-    string f_y_test = argv[5];
-
-    // mnist
-    MNIST mnist(f_x_train, f_x_test, f_y_train, f_y_test);
-
-    unsigned int n_class = mnist.get_n_cls();
-
-    Layer_ *layer_1 = new ConvLayer2d_(cnc::BATCH_SIZE,
-                                       cnc::MNIST_WIDTH, cnc::MNIST_HEIGHT,
-                                       cnc::C_IN, cnc::C_OUT,
-                                       cnc::KERNEL_WIDTH, cnc::KERNEL_HEIGHT,
-                                       cnc::STRIDE_X, cnc::STRIDE_Y,
-                                       cnc::PADDING_X, cnc::PADDING_Y,
-                                       cnc::CONV_ACTIVATION,
-                                       cnc::CONV_GRAD_ACTIVATION);
-
-    Layer_ *layer_2 = new SoftMaxLayer_(cnc::BATCH_SIZE,
-                                        layer_1->get_n_out(),
-                                        n_class);
-
-    vector<Layer_ *> v{layer_1, layer_2};
-
-    std::cout << "conv:n_out:" << layer_1->get_n_out() << std::endl;
-
-    // optimize
-    optimize_(mnist, v, cnc::LEARNING_RATE, cnc::BATCH_SIZE, cnc::N_ITERATION,
-              n_class, argv[6], argv[7]);
-
-    // release
-    delete layer_1;
-    delete layer_2;
-
-}
-
-
-void mnist_conv_pool_eigen(char *argv[]) {
-
-    string f_x_train = argv[2];
-    string f_x_test = argv[3];
-    string f_y_train = argv[4];
-    string f_y_test = argv[5];
-
-    // mnist
-    MNIST mnist(f_x_train, f_x_test, f_y_train, f_y_test);
-
-    unsigned int n_class = mnist.get_n_cls();
-
-    GridLayer2d_ *layer_1 = new ConvLayer2d_(cnc::BATCH_SIZE,
-                                             cnc::MNIST_WIDTH,
-                                             cnc::MNIST_HEIGHT,
-                                             cnc::C_IN, cnc::C_OUT,
-                                             cnc::KERNEL_WIDTH,
-                                             cnc::KERNEL_HEIGHT,
-                                             cnc::STRIDE_X, cnc::STRIDE_Y,
-                                             cnc::PADDING_X, cnc::PADDING_Y,
-                                             cnc::CONV_ACTIVATION,
-                                             cnc::CONV_GRAD_ACTIVATION);
-
-    Layer_ *layer_2 = new MaxPoolLayer2d_(cnc::BATCH_SIZE,
-                                          layer_1->get_output_width(),
-                                          layer_1->get_output_height(),
-                                          cnc::C_OUT,
-                                          cnc::KERNEL_WIDTH, cnc::KERNEL_HEIGHT,
-                                          cnc::PADDING_X, cnc::PADDING_Y);
-
-    Layer_ *layer_3 = new SoftMaxLayer_(cnc::BATCH_SIZE,
-                                        layer_2->get_n_out(),
-                                        n_class);
-
-    vector<Layer_ *> v{layer_1, layer_2, layer_3};
-
-    // optimize
-    optimize_(mnist, v, cnc::LEARNING_RATE, cnc::BATCH_SIZE, cnc::N_ITERATION,
-              n_class, argv[6], argv[7]);
-    // release
-    delete layer_1;
-    delete layer_2;
-    delete layer_3;
+    return {
+            {xmlkey::IS_WEIGHT_RAND_INIT_ENABLED, elem->FirstChildElement(
+                    xmlkey::IS_WEIGHT_RAND_INIT_ENABLED)->GetText()},
+            {xmlkey::WEIGHT_CONSTANT_VALUE,       elem->FirstChildElement(
+                    xmlkey::WEIGHT_CONSTANT_VALUE)->GetText()},
+            {xmlkey::IS_DROPOUT_ENABLED,          elem->FirstChildElement(
+                    xmlkey::IS_DROPOUT_ENABLED)->GetText()},
+            {xmlkey::DROPOUT_RATE,                elem->FirstChildElement(
+                    xmlkey::DROPOUT_RATE)->GetText()},
+    };
 
 }
 
-void shape_map_fc(char **argv) {
+std::map<string, string> params_layer(XMLElement *elem) {
 
-    ShapeMapSet shape_map_set(argv[2], argv[3]);
+    std::map<string, string> &&map = params_common(elem);
+    map[xmlkey::N_HIDDEN] = elem->FirstChildElement(
+            xmlkey::N_HIDDEN)->GetText();
+    map[xmlkey::ACTIVATION_ID] = elem->FirstChildElement(
+            xmlkey::ACTIVATION_ID)->GetText();
 
-    // shuffle
-    ShapeMapSet::shuffle(shape_map_set.x_train, shape_map_set.y_train);
-    ShapeMapSet::shuffle(shape_map_set.x_test, shape_map_set.y_test);
+    return map;
 
-    unsigned int batch_size = 1;
-    unsigned int input_size = shape_map_set.data_size();
-    unsigned int n_hidden_units = 800;
-    unsigned int n_class = shape_map_set.get_n_cls();
-    unsigned int n_iter = 1000000;
+};
 
-    float learning_rate = 0.001f;
+std::map<string, string> params_softmax(XMLElement *elem) {
+    return params_common(elem);
+};
 
-    float (*const layer_activation)(float) = sigmoid;
+Layer_ *new_layer(XMLElement *xml_layer, int n_data, int n_in) {
 
-    float (*const layer_grad_activation)(float) = g_sigmoid;
+    if (strncmp(xml_layer->Name(), xmlkey::FULL_CONNECT, 2) != 0) {
+        error_and_exit("new_layer(): a xml element in arguments is incorrect.");
+    }
 
-    Layer_ *layer_1 = new Layer_(batch_size,
-                                 input_size,
-                                 n_hidden_units,
-                                 layer_activation,
-                                 layer_grad_activation);
-//
-//    Layer_ *layer_2 = new Layer_(batch_size,
-//                                 layer_1->get_n_out(),
-//                                 n_hidden_units,
-//                                 layer_activation,
-//                                 layer_grad_activation);
+    std::map<string, string> &&map = params_layer(xml_layer);
 
-    Layer_ *layer_3 = new SoftMaxLayer_(batch_size,
-                                        layer_1->get_n_out(),
-                                        n_class);
+    // n-hidden
+    int n_hidden = atoi(map[xmlkey::N_HIDDEN].c_str());
+    // activation
+    ACTIVATION act = ActivationHelper::get_activation(
+            atoi(map[xmlkey::ACTIVATION_ID].c_str()));
+    // grad-activation
+    ACTIVATION g_act = ActivationHelper::get_g_activation(
+            atoi(map[xmlkey::ACTIVATION_ID].c_str()));
 
-    vector<Layer_ *> layers{layer_1, layer_3};
+    // weight initialization setting
+    bool is_weight_rand_init_enabled = std::strncmp(
+            map[xmlkey::IS_WEIGHT_RAND_INIT_ENABLED].c_str(), "true", 4) == 0;
+    float weight_constant_value = std::atof(
+            map[xmlkey::WEIGHT_CONSTANT_VALUE].c_str());
 
-    // optimize
-    optimize_(shape_map_set, layers, learning_rate, batch_size, n_iter,
-              n_class, argv[4], argv[5]);
+    // dropout setting
+    bool is_dropout_enabled =
+            std::strncmp(map[xmlkey::IS_DROPOUT_ENABLED].c_str(), "true", 4) ==
+            0;
+    float dropout_rate = std::atof(map[xmlkey::DROPOUT_RATE].c_str());
 
-    // release
-    delete layer_1;
-//    delete layer_2;
-    delete layer_3;
+    return new Layer_(n_data, n_in, n_hidden, act, g_act,
+                      is_weight_rand_init_enabled, weight_constant_value,
+                      is_dropout_enabled, dropout_rate);
 
 }
 
+SoftMaxLayer_ *new_softmax_layer(XMLElement *xml_layer, int n_data, int n_in,
+                                 int n_class) {
 
-typedef void (*func_mnist)(char **);
+    if (strncmp(xml_layer->Name(), xmlkey::SOFTMAX, 2) != 0) {
+        error_and_exit(
+                "new_softmax_layer(): a xml element in arguments is incorrect.");
+    }
 
+    std::map<string, string> &&map = params_softmax(xml_layer);
+
+    // weight initialization setting
+    bool is_weight_rand_init_enabled = std::strncmp(
+            map[xmlkey::IS_WEIGHT_RAND_INIT_ENABLED].c_str(), "true", 4) == 0;
+    float weight_constant_value = std::atof(
+            map[xmlkey::WEIGHT_CONSTANT_VALUE].c_str());
+
+    // dropout setting
+    bool is_dropout_enabled =
+            std::strncmp(map[xmlkey::IS_DROPOUT_ENABLED].c_str(), "true", 4) ==
+            0;
+    float dropout_rate = std::atof(map[xmlkey::DROPOUT_RATE].c_str());
+
+    return new SoftMaxLayer_(n_data, n_in, n_class, is_weight_rand_init_enabled,
+                             weight_constant_value, is_dropout_enabled,
+                             dropout_rate);
+
+}
 
 // コマンドライン引数にmnistへのパスを渡す
 int main(int argc, char *argv[]) {
 
-    std::map<string, func_mnist> functions;
+    /*
+     * XML Parse
+     */
 
-    functions["full_connect_eigen"] = mnist_full_connect_eigen;
-    functions["conv_eigen"] = mnist_conv_eigen;
-    functions["conv_pool_eigen"] = mnist_conv_pool_eigen;
+    XMLDocument xml;
+    xml.LoadFile(argv[1]);
 
-    functions["shape_map_fc"] = shape_map_fc;
+    // xml:root
+    XMLElement *xml_root = xml.FirstChildElement("root");
 
-    if (argc == 0) {
-        std::cerr << "The number of command line arguments is incorrect." <<
-        std::endl;
-        exit(1);
+    // xml:n_iteration
+    XMLElement *xml_n_iteration = xml_root->FirstChildElement("n_iteration");
+    // xml:learning_rate
+    XMLElement *xml_lr = xml_root->FirstChildElement("learning_rate");
+
+    // xml:data_set
+    XMLElement *xml_data_set = xml_root->FirstChildElement("data_set");
+    XMLElement *xml_data_set_id = xml_data_set->FirstChildElement("id");
+    XMLElement *xml_data_set_is_shuffled = xml_data_set->FirstChildElement(
+            "is_shuffled");
+    XMLElement *xml_batch_size = xml_data_set->FirstChildElement("batch_size");
+
+    // xml:layer_params
+    std::vector<std::pair<string, string>> layer_params;
+    XMLElement *xml_nets = xml_root->FirstChildElement("nets");
+
+
+    /*
+     * Getting Start
+     */
+
+    // DataSet
+    std::unique_ptr<DataSet<float, int>> data_set(DataSetHelper::get_dataset(
+            std::atoi(xml_data_set_id->GetText()), argv));
+    // shuffle
+    ShapeMapSet::shuffle(data_set->x_train, data_set->y_train);
+    ShapeMapSet::shuffle(data_set->x_test, data_set->y_test);
+
+    // Parameters for learning
+    int n_class = data_set->get_n_cls(); // N-class
+    int n_iter = std::atoi(xml_n_iteration->GetText()); // N-Iteration
+    int batch_size = std::atoi(xml_batch_size->GetText());
+    float learning_rate = (float) std::atof(xml_lr->GetText());
+
+    // Layers
+    vector<Layer_*> layers;
+    int n_in = data_set->data_size();
+    XMLNode *node = xml_nets->FirstChild();
+    while (node != nullptr) {
+        XMLElement *xml_nets_elem = node->ToElement();
+        if (std::strcmp(xml_nets_elem->Name(), xmlkey::FULL_CONNECT) == 0) {
+            layers.push_back(new_layer(xml_nets_elem, batch_size, n_in));
+        } else if (std::strcmp(xml_nets_elem->Name(), xmlkey::SOFTMAX) == 0) {
+            layers.push_back(new_softmax_layer(xml_nets_elem, batch_size, n_in,
+                                               n_class));
+        } else {
+            error_and_exit("failed to set layer.");
+        }
+        node = node->NextSibling();
+        n_in = layers.back()->get_n_out();
     }
 
-    string function_name = argv[1];
-
-    if (functions.find(function_name) == functions.end()) {
-        std::cerr << "function name is incorrect." << std::endl;
-        exit(1);
-    } else {
-        functions[function_name](argv);
-    }
+    // optimize
+    optimize_(*data_set, layers, learning_rate, batch_size, n_iter,
+              n_class, argv[argc - 2], argv[argc - 1]);
 
 
 }
