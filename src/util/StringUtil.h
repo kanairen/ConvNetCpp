1b5b2bf3d69f04e57095d25f4da56313a10eb377
kanairen <turutekab@gmail.com>
2016-10-01 19:56:31 +0900

    add 文字列からbool値を取得するatob()を追加

--- src/util/StringUtil.h
+++ src/util/StringUtil.h
@@ -5,4 +5,16 @@
 #ifndef CONVNETCPP_STRINGUTIL_H
 #define CONVNETCPP_STRINGUTIL_H
 
+bool atob(const char c[]) {
+    if (std::strncmp(c, "true", 4) == 0) {
+        return true;
+    } else if (std::strncmp(c, "false", 5) == 0) {
+        return false;
+    } else {
+        error_and_exit("atob() : failed to convert an argument to boolean.");
+    }
+    return false;
+}
+
+
 #endif //CONVNETCPP_STRINGUTIL_H
